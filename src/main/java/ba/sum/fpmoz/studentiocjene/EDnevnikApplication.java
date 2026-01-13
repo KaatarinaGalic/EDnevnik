dc658e3d528f446429c8bbe4326b365fd21f1631
KaatarinaGalic <katarina.galic@fpmoz.sum.ba>
2026-01-13 17:19:14 +0100

    Spring Boot,Swagger,MySQL

---
+++ src/main/java/ba/sum/fpmoz/studentiocjene/EDnevnikApplication.java
@@ -0,0 +1,13 @@
+package ba.sum.fpmoz.studentiocjene;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EDnevnikApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EDnevnikApplication.class, args);
+    }
+
+}
