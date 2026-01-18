a8ad63a8d36f961b4d01ea259df38274aeb23480
KaatarinaGalic <katarina.galic@fpmoz.sum.ba>
2026-01-18 16:25:20 +0100

    Dodavanje izmjena

--- src/main/java/ba/sum/fpmoz/studentiocjene/repository/RoleRepository.java
+++ src/main/java/ba/sum/fpmoz/studentiocjene/repository/RoleRepository.java
@@ -2,9 +2,10 @@ package ba.sum.fpmoz.studentiocjene.repository;
 
 import ba.sum.fpmoz.studentiocjene.models.Role;
 import org.springframework.data.jpa.repository.JpaRepository;
-
+import org.springframework.stereotype.Repository;
 import java.util.Optional;
 
-public interface RoleRepository extends JpaRepository<Role,Integer> {
+@Repository
+public interface RoleRepository extends JpaRepository<Role, Long> { // Popravljena zagrada >
     Optional<Role> findByName(String name);
 }
--- src/main/java/ba/sum/fpmoz/studentiocjene/repository/UserRepository.java
+++ src/main/java/ba/sum/fpmoz/studentiocjene/repository/UserRepository.java
@@ -2,10 +2,12 @@ package ba.sum.fpmoz.studentiocjene.repository;
 
 import ba.sum.fpmoz.studentiocjene.models.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
 import java.util.Optional;
 
-public interface UserRepository extends JpaRepository<User,Integer> {
-    Optional<User> findByName(String name);
-
-    Optional<Object> findByEmail(String email);
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+    Optional<User> findByEmail(String email);
+    Optional<User> findByRefreshToken(String refreshToken);
 }
